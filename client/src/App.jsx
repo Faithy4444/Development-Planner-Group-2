f49558452210c63b7a309f1771b03646f28c60f0
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-11-20 22:56:51 +0000

    LAnding page: added routing for landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
-// Import your page components
 import LandingPage from './pages/LandingPage';
 import CreateGoalPage from './pages/CreateGoalPage';
-
-// Import any global styles you need
-// import './styles/global.css';
 
 function App() {
   return (
     <Routes>
-      {/* Route for the main landing page */}
       <Route path="/" element={<LandingPage />} />
-
-      {/* Route for the goal creation form */}
       <Route path="/create-goal" element={<CreateGoalPage />} />
-
-      {/* You can add more pages here later, like the dashboard */}
-      {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
     </Routes>
   );
 }
