5f858737eea40a2c8c487de63a8fbd2d63e2a4eb
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2025-11-22 00:34:04 +0200

    database connected

--- server/db.js
+++ server/db.js
@@ -1,12 +1,21 @@
 import pg from "pg";
 import "dotenv/config";
 const { Pool } = pg;
-export { pool };
+
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+     host: process.env.PGHOST,
+     user: process.env.PGUSER,
+     password: process.env.PGPASSWORD,
+     database: process.env.PGDATABASE,
+     port: process.env.PGPORT,
 });
 
+pool.connect()
+    .then(() => console.log("Connected to PostgreSQL!"))
+    .catch(err => console.error("Connection error:", err));
+
+export { pool };
 
 export async function query(text, params) {
     return pool.query(text, params)
