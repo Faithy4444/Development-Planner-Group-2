c8e824dddda3cd0d73920187ae9511b5c9147352
Droid-An <andrei.filippov.aaf@gmail.com>
2025-11-22 16:06:29 +0000

    changed env variable names

--- server/db.js
+++ server/db.js
@@ -2,23 +2,21 @@ import pg from "pg";
 import "dotenv/config";
 const { Pool } = pg;
 
-
 const pool = new Pool({
-     host: process.env.PGHOST,
-     user: process.env.PGUSER,
-     password: process.env.PGPASSWORD,
-     database: process.env.PGDATABASE,
-     port: process.env.PGPORT,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
 });
 
-pool.connect()
-    .then(() => console.log("Connected to PostgreSQL!"))
-    .catch(err => console.error("Connection error:", err));
+pool
+  .connect()
+  .then(() => console.log("Connected to PostgreSQL!"))
+  .catch((err) => console.error("Connection error:", err));
 
 export { pool };
 
 export async function query(text, params) {
-    return pool.query(text, params)
-};
-
-
+  return pool.query(text, params);
+}
