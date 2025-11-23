c0d44a1db73968f3f707d978f1e58baa5e5c5f77
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2025-11-24 01:46:31 +0200

    api with just users tested

---
+++ server/src/routes/userRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getFullUserData } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/:id/full", getFullUserData);
+
+export default router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import userRoute from "./routes/userRoute.js";
 import { pool } from "./db.js";
 
 const app = express();
@@ -10,10 +11,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//Temp root endpoint for testing
-app.get("/", async (req, res) => {
-  const result = await pool.query("SELECT current_database()");
-  res.send(`the database name is ${result.rows[0].current_database}`);
+app.use("/user", userRoute);
+
+
+app.get("/", (req, res) => {
+  res.send("Server is running! Go to /health or /user/:id/full");
 });
 
 app.listen(PORT, () => {
