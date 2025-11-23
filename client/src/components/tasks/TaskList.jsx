c7afff756105f5e9a2aff24ae0f71c246b317aa3
Droid-An <andrei.filippov.aaf@gmail.com>
2025-11-23 17:28:14 +0000

    now task are marked as completed when. you click checkbox

--- client/src/components/tasks/TaskList.jsx
+++ client/src/components/tasks/TaskList.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import './TaskList.css';
+import React, { useState } from "react";
+import "./TaskList.css";
 
 export const TaskList = ({ tasks }) => {
+  // using local state change because I didn't connect backend
+  const [localTasks, setLocalTasks] = useState(tasks);
+
+  const toggleTask = (id) => {
+    setLocalTasks((prev) =>
+      prev.map((task) =>
+        task.id === id ? { ...task, is_completed: !task.is_completed } : task
+      )
+    );
+  };
+
   return (
     <ul className="task-list">
-      {tasks.map(task => (
-        <li key={task.id} className={task.is_completed ? 'completed' : ''}>
-          <input type="checkbox" defaultChecked={task.is_completed} />
+      {localTasks.map((task) => (
+        <li key={task.id} className={task.is_completed ? "completed" : ""}>
+          <input
+            type="checkbox"
+            checked={task.is_completed}
+            onChange={() => toggleTask(task.id)}
+          />
           <span>{task.title}</span>
         </li>
       ))}
