00ca65505395fe19e0a7ff114d78bd8b22ea9f6f
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2025-11-28 14:58:32 +0200

    task routers and controullers added

--- server/src/controllers/tasksController.js
+++ server/src/controllers/tasksController.js
@@ -16,3 +16,89 @@ export const createTask = async (req, res) => {
         res.status(500).send("Server error")
     }
 }
+
+//Get all tasks for a user
+
+export const getAllTasks = async (req, res) => {
+    const { user_id } = req.params;
+
+    try {
+        const results = await pool.query(
+            `SELECT * FROM tasks 
+            WHERE use_id = $1
+            ORDER BY due_date ASC`,
+            [user_id]
+        );
+        res.status(200).json(results.rows);
+    } catch (err) {
+        console.error("getAllTasks error:", err);
+        res.status(500).json({ message: "Server error fetching tasks." });
+    }
+};
+
+export const getTaskById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const results = await pool.query(
+            `SELECT * FROM tasks WHERE ID = $1`, [id]
+        );
+        if (results.rows.length === 0) {
+            return res.status(404).json({ message: "Task not found." });
+        };
+    } catch (err) {
+        console.error("getTaskById error:", err);
+        res.status(500).json({ message: "Server error fetching task." });
+    }
+};
+
+//update tasks 
+
+export const updateTasks = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, due_date, is_completed } = req.body;
+
+    try {
+        const results = await pool.query(
+            `UPDATE tasks 
+             SET title = COALESCE($1, title),
+                 description = COALESCE($2, description),
+                 due_date = COALESCE($3, due_date),
+                 completed = COALESCE($4, completed)
+             WHERE id = $5
+             RETURNING *`,
+      [title, description, due_date, completed, id]
+        );
+        
+        if (results.rows.length === 0) {
+          return res.status(404).json({ message: "Task not found." });
+    }
+
+    res.status(200).json(result.rows[0]);
+    }catch(err) {
+    console.error("updateTask error:", err);
+    res.status(500).json({ message: "Server error updating task." });
+  }
+};
+
+//Deleting task
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      `DELETE FROM tasks WHERE id = $1 RETURNING *`,
+      [id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Task not found." });
+    }
+
+    res.status(200).json({ message: "Task deleted successfully." });
+  } catch (err) {
+    console.error("deleteTask error:", err);
+    res.status(500).json({ message: "Server error deleting task." });
+  }
+};
--- server/src/routes/tasksRoute.js
+++ server/src/routes/tasksRoute.js
@@ -1,8 +1,19 @@
 import express from "express";
 import { getFullUserData } from "../controllers/userController.js";
+import {
+    createTask,
+    getAllTasks,
+    getTaskById,
+    updateTasks,
+    deleteTask,
+} from "../controllers/tasksController.js"
 
 const router = express.Router();
 
-router.get("/:id/full", getFullUserData);
+router.post("/", createTask);
+router.get("/user/:user_id", getAllTasks);
+router.get("/:id", getTaskById);
+router.put("/:id", updateTasks);
+router.delete("/:id", deleteTask);
 
 export default router
