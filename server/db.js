2c95dd7d2f73534ac868f84941d4aa1950f9e287
Droid-An <andrei.filippov.aaf@gmail.com>
2025-11-29 22:52:40 +0000

    refactor: simplify database connection configuration using DATABASE_URL

--- server/db.js
+++ server/db.js
@@ -3,11 +3,7 @@ import "dotenv/config";
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
+  connectionString: process.env.DATABASE_URL,
 });
 
 pool
