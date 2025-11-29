e64736ceb506502013c71a1bbdde09b392f0ab82
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2025-11-29 15:02:07 +0000

    userRouer added to server

--- server/src/server.js
+++ server/src/server.js
@@ -17,7 +17,7 @@ app.use('/api/auth', require('./routes/userRoute'));
 app.use("/api/user", userRoute);
 app.use("/api/goals", goalsRoutes);
 app.use("/api/tasks", taskRoutes);
-
+app.use('/api/users', userRouter); 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
