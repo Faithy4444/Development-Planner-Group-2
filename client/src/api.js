274cef0d0e7ed07825e80f470fa0e54cac4bd0c5
Droid-An <andrei.filippov.aaf@gmail.com>
2025-11-30 10:11:24 +0000

    changed api url variable to match the deployed env var

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 const API_BASE = import.meta.env.DEV
   ? "/api" // uses Vite proxy in development
-  : import.meta.env.API_URL; // real backend in production
+  : import.meta.env.VITE_API_URL; // real backend in production
 
 export const apiUrl = (endpoint) => `${API_BASE}${endpoint}`;
 console.log(apiUrl);
