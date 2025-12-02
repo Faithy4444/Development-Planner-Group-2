96364935478eab0eb80d41e37eaf0f8e6ed1d8e6
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2025-12-02 12:13:44 +0200

    initial nodemailer services

---
+++ server/services/emailServices.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
+
+export const sendEmail = async (to, subject, text) => {
+  try {
+    await transporter.sendMail({
+      from: `"Goal Reminder" <${process.env.SMTP_USER}>`,
+      to,
+      subject,
+      text
+    });
+    console.log(`Email sent to ${to}: ${subject}`);
+  } catch (err) {
+    console.error("Error sending email:", err);
+  }
+};
