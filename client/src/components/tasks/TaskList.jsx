087713422a38d67af201ae0b4f1f51eeeac6771d
Droid-An <andrei.filippov.aaf@gmail.com>
2025-12-02 20:01:15 +0000

    added meatball menu with a dropdown

--- client/src/components/tasks/TaskList.jsx
+++ client/src/components/tasks/TaskList.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskList.css";
 import { useFetch } from "../../useFetch";
 
 export const TaskList = ({ tasks, onToggle }) => {
   const { executeFetch } = useFetch();
+  const [openMenuId, setOpenMenuId] = useState(null);
 
   const checkTask = async (id) => {
     const data = await executeFetch(`/api/tasks/${id}`, "PUT");
@@ -11,17 +12,38 @@ export const TaskList = ({ tasks, onToggle }) => {
       onToggle(id);
     }
   };
+  const toggleMenu = (id) => {
+    setOpenMenuId(openMenuId === id ? null : id);
+  };
 
   return (
     <ul className="task-list">
       {tasks.map((task) => (
-        <li key={task.id} className={task.is_completed ? "completed" : ""}>
-          <input
-            type="checkbox"
-            checked={task.is_completed}
-            onChange={() => checkTask(task.id)}
-          />
-          <span>{task.title}</span>
+        <li
+          key={task.id}
+          className={`task-item ${task.is_completed ? "completed" : ""}`}
+        >
+          <div className="left-content">
+            <input
+              type="checkbox"
+              checked={task.is_completed}
+              onChange={() => checkTask(task.id)}
+            />
+            <span>{task.title}</span>
+          </div>
+
+          <div className="menu-wrapper">
+            <button className="menu-button" onClick={() => toggleMenu(task.id)}>
+              ⋯
+            </button>
+
+            {openMenuId === task.id && (
+              <div className="dropdown-menu">
+                <button>Edit</button>
+                <button>Delete</button>
+              </div>
+            )}
+          </div>
         </li>
       ))}
     </ul>
