bb345556e76c4ed6077ecb86830233f0a73a2052
Faithy4444 <161722786+Faithy4444@users.noreply.github.com>
2025-12-05 23:09:33 +0200

    duplicate deleted

--- server/src/controllers/tasksController.js
+++ server/src/controllers/tasksController.js
@@ -136,25 +136,3 @@ export const deleteTask = async (req, res) => {
     res.status(500).send("Server Error");
   }
 };
-
-export const completeTask = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const results = await pool.query(
-      `UPDATE tasks 
-             SET is_completed = NOT is_completed
-             WHERE id = $1
-             RETURNING *`,
-      [id]
-    );
-
-    if (results.rows.length === 0) {
-      return res.status(404).json({ message: "Task not found." });
-    }
-
-    res.status(200).json("ok");
-  } catch (err) {
-    console.error("updateTask error:", err);
-    res.status(500).json({ message: "Server error updating task." });
-  }
-};
